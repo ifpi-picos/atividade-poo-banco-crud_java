d6320f362656743e6ffecd67376ccedf4ecba735
Lander16-Programmer <landeilsonveloso2022@gmail.com>
2022-10-06 19:51:38 -0300

    novas classes

---
+++ src/br/edu/ifpi/dominio/ContaCorrente.java
@@ -0,0 +1,15 @@
+package br.edu.ifpi.dominio;
+
+public class ContaCorrente {
+    private double chequeEspecial;
+
+    public ContaCorrente(double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public double getChequeEspecial() {
+        return this.chequeEspecial;
+    }
+
+}
+
---
+++ src/br/edu/ifpi/dominio/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package br.edu.ifpi.dominio;
+
+public class ContaPoupanca {
+    private double rendimento;
+
+    public ContaPoupanca (double rendimento){
+        this.rendimento = rendimento;
+    }
+    public double getRendimento(){
+        return this.rendimento;
+    }
+}
+
+
